47fb92e19e9d79339dc38eb1828cd9cd2e5b0c7a
smarzic <sanjin.marzic@gmail.com>
2022-12-11 11:51:04 +0100

    Novi projekt (Empty activity), početna aktivnost

---
+++ app/src/main/java/hr/dante_ri/kviz/MainActivity.java
@@ -0,0 +1,14 @@
+package hr.dante_ri.kviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
